5ff7fcf791f0d933a23de212e0da1b1996d86c4e
SuperPupperDoggo <65412993+SuperPupperDoggo@users.noreply.github.com>
2026-07-03 14:55:49 -0400

    Update DeathListener.java

--- src/main/java/de/zekro/hcrevive/listeners/DeathListener.java
+++ src/main/java/de/zekro/hcrevive/listeners/DeathListener.java
@@ -16,6 +16,7 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
